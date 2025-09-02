190cebc3f7235d4b34ded7ad3dc01cb2e399ce1d
LewieJ08 <lewiej08@gmail.com>
2025-09-02 12:37:30 +0100

    feat: import and use weatherRoutes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mwLogger = require("./middleware/logger");
+const weatherRoutes = require("./routes/weatherRoutes");
 
 require("dotenv").config();
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(mwLogger);
+app.use(weatherRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);

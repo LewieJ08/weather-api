ee728d66a839211898f681557c9c3a92d08f205b
LewieJ08 <lewiej08@gmail.com>
2025-09-02 10:51:19 +0100

    fix: use urlencoded

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use()
+app.use(express.urlencoded({extended: true}));
+app.use(mwLogger);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);

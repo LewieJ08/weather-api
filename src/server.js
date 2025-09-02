9c4d12922e567f61752b869671795a70e1cbfc88
LewieJ08 <lewiej08@gmail.com>
2025-09-02 10:45:28 +0100

    feat: use logger middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const mwLogger = require("./middleware/logger");
 
 require("dotenv").config();
 const app = express();
+
+app.use(express.json());
+app.use()
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);

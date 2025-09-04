877d27d465a8af2c9397b84a3d59aab2e1b61c0c
LewieJ08 <lewiej08@gmail.com>
2025-09-04 12:28:02 +0100

    feat: basic weather service api setup

---
+++ src/services/weatherService.js
@@ -0,0 +1,4 @@
+module.exports = location => {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=us&key=${process.env.API_KEY}&contentType=json`;
+    fe
+}

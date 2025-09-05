3e05117e5cb388201ea380927a2587f3a8a76ca5
LewieJ08 <lewiej08@gmail.com>
2025-09-05 12:51:12 +0100

    feat: filter down rawData from weather api

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,30 @@
 module.exports = async location => {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${process.env.API_KEY}&contentType=json`;
-    const response = await fetch(url);
+    try {
+        const response = await fetch(url, {
+            method: "GET"
+        });
 
-    return response;
+        if (!response.ok) {
+            throw new Error({status: response.status});
+        }
+
+        const rawData = await response.json();
+        const days = [];
+        
+        for (day of rawData.days) {
+            if (!days[9]) {
+                days.push({
+                    dateTime: day.datetime,
+                    tempMax: day.tempmax,
+                 });
+            } else {
+                break;
+            }
+        }
+
+        return days;
+    } catch(error) {
+        throw error;
+    }
 }

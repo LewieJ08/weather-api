ac8edf266d5dad41900ccc6fe89d11af2c9efa06
LewieJ08 <lewiej08@gmail.com>
2025-09-07 22:06:48 +0100

    refactor: add PORT default value

--- src/server.js
+++ src/server.js
@@ -4,12 +4,13 @@ const weatherRoutes = require("./routes/weatherRoutes");
 
 require("dotenv").config();
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(mwLogger);
 app.use(weatherRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
